d47e9d55c4d10312519aa8205d1298486ff9a029
Robert Sandor <sandorrobertk94@gmail.com>
2025-09-17 18:51:49 +0300

    fish - small cleanup and theme update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,28 +7,13 @@ command -q brew; and fish_add_path (brew --prefix rustup)/bin
 if status --is-interactive
     fish_vi_key_bindings
 
-    fish_config theme choose "Catppuccin Frappe"
+    fish_config theme choose "Catppuccin Mocha"
 
     set -gx SHELL (which fish)
     set -gx EDITOR nvim
     set -gx MANPAGER "nvim +Man!"
 
     set -l fish_conf_dir ~/.config/fish
-
-    function sshf
-        set selected (cat ~/.ssh/known_hosts | cut -d ' ' -f1 | uniq | fzf-tmux -p -- -q "$argv[1]")
-        test -n "$selected"; and ssh -t "$selected"
-    end
-
-    function sshrm
-        set selected (cat ~/.ssh/known_hosts | cut -d ' ' -f1 | uniq | fzf-tmux -p -- -q "$argv[1]")
-        test -n "$selected"; and ssh-keygen -R "$selected"
-    end
-
-    # Aliases
-    function tm -d "tmux sessionizer"
-        command ~/.config/tmux/sessionizer.sh paths
-    end
 
     # Interactive shell initialisation
     command -q brew; and source "$fish_conf_dir/brew.fish"
@@ -38,6 +23,7 @@ if status --is-interactive
     command -q starship; and starship init fish | source; and enable_transience
     command -q mise; and mise activate fish | source
     command -q yazi; and source "$fish_conf_dir/yazi.fish"
+    command -q bw; and source "$fish_conf_dir/bitwarden.fish"
 
     # abbreviations
     source "$fish_conf_dir/abbrs.fish"
