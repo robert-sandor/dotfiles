9f0120809650e7ae39152978f33dc13a7f50661b
Robert Sandor <sandorrobertk94@gmail.com>
2026-02-11 23:37:47 +0200

    fzf - move default opts to rc file

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,12 +28,7 @@ for f in /opt/homebrew/bin /home/linuxbrew/.linuxbrew/bin
 end
 
 if command -q fzf
-  set -gx FZF_DEFAULT_OPTS "\
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-    --color=selected-bg:#45475a"
-
+  set -gx FZF_DEFAULT_OPTS_FILE ~/.config/fzfrc
   fzf --fish | source
 end
 
