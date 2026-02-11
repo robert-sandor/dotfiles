210348b7e80f004d9ab7e8739d90ab3c8ffb5da3
Robert Sandor <sandorrobertk94@gmail.com>
2026-02-11 23:25:20 +0200

    fish - remove some more unnecessary functions and improve bw socket function

--- dot_config/private_fish/functions/set_bitwarden_ssh_agent.fish
+++ dot_config/private_fish/functions/set_bitwarden_ssh_agent.fish
@@ -1,33 +1,14 @@
 function set_bitwarden_ssh_agent
-    set -l os_type (uname)
-    set -l socket_path ""
+  # Possible locations are for: macos appstore, linux flatpak, macos dmg and linux package
+    set -l possible_socket_locations \
+      "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock" \
+      "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock" \
+      "$HOME/.bitwarden-ssh-agent.sock"
 
-    switch $os_type
-        case Darwin
-            # macOS: Check for App Store version first, then .dmg version
-            set -l appstore_socket "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
-            set -l dmg_socket "$HOME/.bitwarden-ssh-agent.sock"
-
-            if test -S $appstore_socket
-                set socket_path $appstore_socket
-            else if test -S $dmg_socket
-                set socket_path $dmg_socket
-            end
-
-        case Linux
-            # Linux: Check for Flatpak version first, then standard version
-            set -l flatpak_socket "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
-            set -l standard_socket "$HOME/.bitwarden-ssh-agent.sock"
-
-            if test -S $flatpak_socket
-                set socket_path $flatpak_socket
-            else if test -S $standard_socket
-                set socket_path $standard_socket
-            end
-    end
-
-    # Set the SSH_AUTH_SOCK environment variable
-    if test -n "$socket_path"
-        set -gx SSH_AUTH_SOCK $socket_path
+    for socket in $possible_socket_locations
+      if test -S $socket
+        set -gx SSH_AUTH_SOCK "$socket"
+        break
+      end
     end
 end
--- dot_config/private_fish/functions/tm.fish
+++
@@ -1,3 +0,0 @@
-function tm -d "tmux sessionizer"
-    command ~/.config/tmux/sessionizer.sh paths
-end
