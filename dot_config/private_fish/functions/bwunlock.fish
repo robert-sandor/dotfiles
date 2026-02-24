436bd28be9b51c86a59e4eb9daabf56eefb413ce
Robert Sandor <sandorrobertk94@gmail.com>
2026-02-25 01:54:05 +0200

    fish - fix bwunlock redirect typo

--- dot_config/private_fish/functions/bwunlock.fish
+++ dot_config/private_fish/functions/bwunlock.fish
@@ -2,7 +2,7 @@ if command -q bw
   function bwunlock
     set -l bw_session (bw unlock --raw)
     if test -z "$bw_session"
-      echo "Failed to open vault" >$2
+      echo "Failed to open vault" >&2
     else
       set -Ux BW_SESSION "$bw_session"
       trap bwlock EXIT
